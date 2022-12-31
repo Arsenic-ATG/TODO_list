663183666f987ef56d6ba99510c2e99a966232b1
Ankur <54987647+Arsenic-ATG@users.noreply.github.com>
2022-12-31 15:44:55 +0530

    enh: Add functionality to delete record from tasks
    - close #3 on GitHub.
    - delete.php: new file ( handle deletion of a
      records form TODO list)

---
+++ delete.php
@@ -0,0 +1,20 @@
+<?php
+if (isset($_GET["id"])){
+    $id = $_GET["id"];
+    echo $id;
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "todo_list";
+
+    // Connect to the sql database
+    $connection = new mysqli($servername, $username, $password, $database);
+
+    $sql_query = "DELETE FROM tasks WHERE id=$id;";
+    $connection->query($sql_query);
+}
+
+header ("LOCATION: ./index.php");
+exit;
+?>
